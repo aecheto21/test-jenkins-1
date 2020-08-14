9d1ecb3644f98a839ad8f24c1957db595f73a36e
Angel <aecheto21@gmail.com>
2020-08-14 16:50:29 +0000

    agregamos la aplicacion test flask

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+def suma(a,b):
+    return a+b
+
+@app.route('/')
+def hello():
+    res = suma(3,2)
+    name = request.args.get("name", "World")
+#    return f'Hello, {escape(name)}! %s' % (res)
+    return "Hello World! %s" % (res)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
